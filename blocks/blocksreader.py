c5fb3345b6482de8f398dc664431e154d64186be
Warlock-Dalbaeb <a.yvarov@yandex.ru>
2017-08-03 02:07:54 +0700

    init first files

---
+++ blocks/blocksreader.py
@@ -0,0 +1,132 @@
+from util import *
+
+class Tx:
+	def __init__(self, blockchain):
+		self.version = uint4(blockchain)
+		self.inCount = varint(blockchain)
+		self.inputs = []
+		for i in range(0, self.inCount):
+			input = txInput(blockchain)
+			self.inputs.append(input)
+		self.outCount = varint(blockchain)
+		self.outputs = []
+		if self.outCount > 0:
+			for i in range(0, self.outCount):
+				output = txOutput(blockchain)
+				self.outputs.append(output)	
+		self.lockTime = uint4(blockchain)
+	def GetAllAsList(self):
+		print self.inputs[0].GetAllAsList()
+		return {
+		"TxInput":self.inputs,
+		"TxOutputs":self.outputs,
+		"TxVersion":self.version,
+		"TxInputs":self.inCount,
+		"TxOutPuts":self.outCount,
+		"TxLockTime":self.lockTime
+		}
+
+class txInput:
+	def __init__(self, blockchain):
+		self.prevhash = hash32(blockchain)
+		self.txOutId = uint4(blockchain)
+		self.scriptLen = varint(blockchain)
+		self.scriptSig = blockchain.read(self.scriptLen)
+		self.seqNo = uint4(blockchain)
+	def GetAllAsList(self):
+	 return {
+		"PrevHash":self.prevhash,
+		"txOutId":self.txOutId,
+		"scriptLen":self.scriptLen,
+		"scriptSig":self.scriptSig,
+		"seqNo":self.seqNo
+		}		
+class txOutput:
+	def __init__(self, blockchain):	
+		self.value = uint8(blockchain)
+		self.scriptLen = varint(blockchain)
+		self.pubkey = blockchain.read(self.scriptLen)
+	def GetAllAsList(self):
+	 return {
+		"value":self.value,
+		"scriptLen":self.scriptLen,
+		"pubkey":self.pubkey
+		}
+
+class BlockHeader:
+	def __init__(self, blockchain):
+		self.version = uint4(blockchain)
+		self.previousHash = hash32(blockchain)
+		self.merkleHash = hash32(blockchain)
+		self.time = uint4(blockchain)
+		self.bits = uint4(blockchain)
+		self.nonce = uint4(blockchain)
+	def GetAllAsList(self):
+		return {
+		"version": self.version,
+		"previousHash": hashStr(self.previousHash),
+		"merkleRoot": hashStr(self.merkleHash),
+		"ntime": self.time,
+		"nbits": self.bits,
+		"nonce": self.nonce	
+		}
+
+class BlockReader:
+	def __init__(self,blockchain,seek=0):
+	 self.endOfFile=True
+	 self.lastSeek = seek
+	 self.magicNum = 0
+	 self.blocksize = 0
+	 self.blockheader = {}
+	 self.txCount = 0
+	 self.Txs = []
+	 if seek != 0:
+		blockhasin.seek(seek)
+	 if self.hasLength(blockchain, 8):	#If has 8 byte length(8 chars)
+		self.magicNum = uint4(blockchain)
+		self.blocksize = uint4(blockchain)
+		self.endOfFile=False
+	 else:			# if end of file
+		self.endOfFile = True
+		return True
+		
+	 if self.hasLength(blockchain, self.blocksize):
+		self.setHeader(blockchain)
+		self.txCount = varint(blockchain)
+		self.Txs = []
+
+		for i in range(0, self.txCount):
+		 tx = Tx(blockchain)
+		 self.Txs.append(tx)
+	 else:
+		 self.lastSeek = blockhain.tell()
+		 self.endOfFile = True
+	
+	def getBlocksize(self):
+		return self.blocksize
+
+	def hasLength(self, blockchain, size):
+		curPos = blockchain.tell()
+		blockchain.seek(0, 2)
+		
+		fileSize = blockchain.tell()
+		blockchain.seek(curPos)
+
+		tempBlockSize = fileSize - curPos
+		self.lastSeek = blockchain.tell()
+		if tempBlockSize < size:
+			return False
+		return True
+
+	def setHeader(self, blockchain):
+		tmpheaderObject = BlockHeader(blockchain)
+		self.blockHeader = tmpheaderObject.GetAllAsList()
+		tmpheaderObject=None
+
+	def ReturnAllToList(self):
+		return {
+			"Magic":self.magicNum,
+			"BlockSize":self.blocksize,
+			"BlockHeader":self.blockHeader,
+			"Txs":self.Txs
+		       }
---
+++ blocks/test.py
@@ -0,0 +1,45 @@
+import blocksreader as bh
+
+def parse(path):
+	blockchain = open(path, 'rb')
+	print 'print Parsing Block Chain'
+	continueParsing = True
+	counter = 0
+	blockchain.seek(0, 2)
+	fSize = blockchain.tell() - 80 #Minus last Block header size for partial file
+	blockchain.seek(0, 0)
+	block = bh.BlockReader(blockchain)
+	while counter < 1000:			
+		continueParsing = not block.endOfFile
+		if continueParsing:
+			tmp = block.ReturnAllToList()
+			print "+"*30
+			print "Magic %8x " % tmp["Magic"]
+			print "BlockSize %d " % tmp["BlockSize"]
+			print "version %d " % tmp["BlockHeader"]['version']
+			print "previousHash %s " % tmp["BlockHeader"]['previousHash']
+			print "Merkle root %s" % tmp['BlockHeader']['merkleRoot']
+			print "Difficulty %8x " % tmp["BlockHeader"]['nbits']
+			print "nonce %s " % tmp["BlockHeader"]['nonce']
+			print "ntime %s " % str(tmp["BlockHeader"]['ntime'])
+			print "~"*15+"TX"+"~"*15
+			for txs in tmp["Txs"]:
+			 tmptxs = txs.GetAllAsList()
+			 print "TxVersion %d" % tmptxs["TxVersion"]
+			 print "#"*15+"INPUTS"+"#"*15
+			 print "Count inputs %s " % tmptxs["TxInputs"]
+			 for i in tmptxs['TxInput']:
+				print i.GetAllAsList()			 
+			 print "@"*15+"OUTPUTS"+"@"*15
+			 print "Count OutPuts %s " % tmptxs["TxOutPuts"]
+			 for i in tmptxs['TxOutputs']:
+				 print i.GetAllAsList()
+			 print "Lock Time %s " % tmptxs["TxLockTime"]
+			print "~"*15+"~~"+"~"*15
+			print "+"*30
+		counter+=1
+
+	print 'Parsed 1000 blocks'
+	print "Last seek %s" % block.lastSeek
+
+parse("/home/lock/.gostcoin/blocks/blk00000.dat")
